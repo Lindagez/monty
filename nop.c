10b6d999d2c35ca28acbe8298ad9419f36dfffc3
Birhanu6 <birhanusisayh@gmail.com>
2022-12-15 08:53:13 -0800

    taskes are done

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+  *f_nop- nothing
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
+
